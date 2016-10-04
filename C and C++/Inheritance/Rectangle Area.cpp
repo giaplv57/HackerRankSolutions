cb464af17771249aeb87ec46b8fb7bd666ea6fe2
giaplv57 <giaplv_57@vnu.edu.vn>
2016-10-04 21:54:30 +0700

    new C++ exercise

---
+++ C and C++/Inheritance/Rectangle Area.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+/*
+ * Create classes Rectanlge and RectangleArea
+ */
+class Rectangle{
+    public:
+        int width, height;
+        void display(){
+            cout << this->width << " " << this->height << endl;
+        }
+};
+class RectangleArea : public Rectangle{
+    public:
+        void read_input(){
+            cin >> this->width >> this->height;
+        }
+        void display(){
+            cout << this->width * this->height;
+        }
+};
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
