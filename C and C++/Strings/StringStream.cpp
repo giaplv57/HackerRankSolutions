76ebb639ed96d3c13c4001b9f6f03374eb5f25b1
giaplv57 <giaplv_57@vnu.edu.vn>
2016-10-28 20:36:08 +0700

    update

--- C and C++/Strings/StringStream.cpp
+++ C and C++/Strings/StringStream.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 
 vector<int> parseInts(string str) {
-    vector<int> temp;
-    stringstream ss;
-    ss << str;
-    int number; char tempChar;
-    while (ss.rdbuf()->in_avail()!=0){
-        ss >> number >> tempChar;
-        temp.push_back(number);
-    }
-    return temp;
-    
+	vector<int> temp;
+	stringstream ss;
+	ss << str;
+	int number; char tempChar;
+	while (ss.rdbuf()->in_avail()!=0){
+		ss >> number >> tempChar;
+		temp.push_back(number);
+	}
+	return temp;
+	
 }
 
 int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
-    }
-    
-    return 0;
+	string str;
+	cin >> str;
+	vector<int> integers = parseInts(str);
+	for(int i = 0; i < integers.size(); i++) {
+		cout << integers[i] << "\n";
+	}
+	
+	return 0;
 }
